130f373bac8fc353584c2ad7affa7362c3c565b9
Stephen-carl <okafor.stephen13@yahoo.com>
2025-08-27 15:01:04 +0100

    quiz setup

---
+++ Hamster/app/(home)/learn/quiz/history/index.tsx
@@ -0,0 +1,12 @@
+import { VStack } from "@/components/ui/vstack";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function QuizHistory() {
+    return(
+        <SafeAreaView className="flex-1 bg-white">
+            <VStack className="flex-1 max-w-full min-h-full ">
+
+            </VStack>
+        </SafeAreaView>
+    )
+}
